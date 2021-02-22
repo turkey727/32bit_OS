2e4849a4c3a939e261d98da00dec4e74376f0a61
turkey727 <turkey0727@outlook.jp>
2021-02-22 15:13:36 +0900

    putc関数で文字を出力できるように

--- boot.s
+++ boot.s
@@ -1,6 +1,8 @@
 	BOOT_LOAD	equ	0x7C00  ;ブートプログラムのロード位置
 	ORG	BOOT_LOAD;		;これで、最初にロードするアドレスをアセンブラに指示
 
+%include	".\include\macro.s"	;cdecl呼び出し規約をマクロで設定
+
 entry:
 	jmp	ipl         		;最初はiplから処理
 
@@ -21,18 +23,20 @@ ipl:
 					;ストレージデバイスの番号が設定されており、
 					;それを指定したアドレスに保存し、今後も使っていく。
 
-;以下3行、ビデオBIOSコールを呼び出すためのレジスタの設定
-	mov	al, 'A'			;alレジスタで出力する文字を指定
-	mov	ah, 0x0E		;ah=0x0Eで、テレタイプ式１文字出力に設定
-	mov	bx, 0x0000		;bxレジスタでページ番号と文字色を指定
-
-	int	0x10			;ビデオBIOSコールを呼び出す
+	cdecl	putc, word 'H'
+	cdecl	putc, word 'e'
+	cdecl	putc, word 'l'
+	cdecl	putc, word 'l'
+	cdecl	putc, word 'o'
+	cdecl	putc, word '!'
 
 	jmp	$;
 
 ALIGN 2, db 0
 BOOT:
 .DRIVE:	dw 0;
+
+%include	".\modules\real\putc.s"	;putc.sを使えるように
 
 	times 	510 - ($ - $$) db 0x00;
 	db	0x55, 0xAA;
