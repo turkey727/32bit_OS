72d30ebc58260a0aecbc4b58398177f7b38ebd91
turkey727 <turkey0727@outlook.jp>
2021-02-22 13:20:45 +0900

    後に実装するBPB領域の作成

--- boot.s
+++ boot.s
@@ -1,5 +1,11 @@
 entry:
+	jmp	ipl		;最初はipl(ブートプログラム)から開始
+
+	times	90 - ($ - $$) db 0x90		;BPLと呼ばれる領域。後で記述するため、
+						;今はとりあえず90byte分のNOPで埋める
+	
+ipl:
 	jmp	$;
 
-	times	510 - ($ - $$) db 0x00
-	db	0x55, 0xAA
+	times 	510 - ($ - $$) db 0x00;
+	db	0x55, 0xAA;
