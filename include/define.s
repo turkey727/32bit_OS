b88fc4fd8174cc616f52fe162b8c21ab4502eaf9
turkey727 <turkey0727@outlook.jp>
2021-03-01 10:42:57 +0900

    メモリ情報を格納する領域のサイズの定義を行う命令の追加、及びコメントの追加

--- include/define.s
+++ include/define.s
@@ -1,5 +1,7 @@
-BOOT_LOAD	equ	0x7C00
+BOOT_LOAD	equ	0x7C00			;ブートプログラムの最初にロードするアドレスの定義
 
-BOOT_SIZE	equ	(1024 * 8)
-SECT_SIZE	equ	(512)
-BOOT_SECT	equ	(BOOT_SIZE / SECT_SIZE)
+BOOT_SIZE	equ	(1024 * 8)		;ブートプログラムのサイズ(8Kバイト)
+SECT_SIZE	equ	(512)			;セクタサイズの定義
+BOOT_SECT	equ	(BOOT_SIZE / SECT_SIZE)	;ブートプログラムのセクタ数の定義
+
+E820_RECORD_SIZE	equ	20		;BIOSコールで取得したメモリ情報を格納する領域のサイズの定義
