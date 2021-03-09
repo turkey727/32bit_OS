a7f61ba2aa71ecf3433748ad1e3b67150f9674e5
turkey727 <turkey0727@outlook.jp>
2021-03-09 16:26:48 +0900

    カーネルに関する情報を定義

--- include/define.s
+++ include/define.s
@@ -1,7 +1,12 @@
 BOOT_LOAD	equ	0x7C00			;ブートプログラムの最初にロードするアドレスの定義
+BOOT_END	equ	(BOOT_LOAD + BOOT_SIZE)
 
 BOOT_SIZE	equ	(1024 * 8)		;ブートプログラムのサイズ(8Kバイト)
 SECT_SIZE	equ	(512)			;セクタサイズの定義
 BOOT_SECT	equ	(BOOT_SIZE / SECT_SIZE)	;ブートプログラムのセクタ数の定義
 
 E820_RECORD_SIZE	equ	20		;BIOSコールで取得したメモリ情報を格納する領域のサイズの定義
+
+KERNEL_LOAD	equ	0x0010_1000		;ロードするアドレス
+KERNEL_SIZE	equ	(1024 * 8)		;カーネルのサイズ
+KERNEL_SECT	equ	(KERNEL_SIZE / SECT_SIZE)	;カーネルのセクタ数の定義
